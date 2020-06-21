918df906dece736396612dbede043ff3c4f38fdc
GhitaAlexandruIon <ghita.alexandru6@gmail.com>
2020-06-21 15:49:56 +0300

    facts field for languages

--- portfolio/models.py
+++ portfolio/models.py
@@ -4,6 +4,7 @@ from ckeditor.fields import RichTextField
 
 class Technology(models.Model):
     name = models.CharField(max_length=30)
+    facts = RichTextField(default=True)
 
     def __str__(self):
         return self.name
--- portfolio/static/portfolio/js/script.js
+++ portfolio/static/portfolio/js/script.js
@@ -1,150 +1,144 @@
+$(document).ready(function () {
 
-$(document).ready(function() {
+    $('#slides').superslides({
+        animation: 'fade',
+        play: 5000,
+        pagination: false
+    });
 
-	$('#slides').superslides({
-		animation: 'fade',
-		play: 5000,
-		pagination: false
-	});
+    let typed = new Typed(".typed", {
+        strings: ["Software Engineer.", "Web Developer.", "Student."],
+        typeSpeed: 70,
+        loop: true,
+        startDelay: 1000,
+        showCursor: false
+    });
 
-	let typed = new Typed(".typed", {
-		strings: ["Software Engineer.", "Web Developer.", "Student."],
-		typeSpeed: 70,
-		loop: true,
-		startDelay: 1000,
-		showCursor: false
-	});
-
-	$('.owl-carousel').owlCarousel({
-	    loop:true,
-	    items: 4,
-	    responsive:{
-	        0:{
-	            items:1
-	        },
-	        480:{
-	            items:2
-	        },
-	        768:{
-	            items:3
-	        },
-	        938:{
-	            items:4
-	        }
-	    }
-	});
-
-
-	
+    $('.owl-carousel').owlCarousel({
+        loop: true,
+        items: 4,
+        responsive: {
+            0: {
+                items: 1
+            },
+            480: {
+                items: 2
+            },
+            768: {
+                items: 3
+            },
+            938: {
+                items: 4
+            }
+        }
+    });
 
 
-	let skillsTopOffset = $(".skillsSection").offset().top;
-	let statsTopOffset = $(".statsSection").offset().top;
-	let countUpFinished = false;
+    let skillsTopOffset = $(".skillsSection").offset().top;
+    let statsTopOffset = $(".statsSection").offset().top;
+    let countUpFinished = false;
 
-	$(window).scroll(function() {
+    $(window).scroll(function () {
 
-		if(window.pageYOffset > skillsTopOffset - $(window).height() + 200) {
+        if (window.pageYOffset > skillsTopOffset - $(window).height() + 200) {
 
-			$('.chart').easyPieChart({
-		        easing: 'easeInOut',
-		        barColor: '#fff',
-		        trackColor: false,
-		        scaleColor: false,
-		        lineWidth: 4,
-		        size: 152,
-		        onStep: function(from, to, percent) {
-		        	$(this.el).find('.percent').text(Math.round(percent));
-		        }
-		    });
-
-
-		}
+            $('.chart').easyPieChart({
+                easing: 'easeInOut',
+                barColor: '#fff',
+                trackColor: false,
+                scaleColor: false,
+                lineWidth: 4,
+                size: 152,
+                onStep: function (from, to, percent) {
+                    $(this.el).find('.percent').text(Math.round(percent));
+                }
+            });
 
 
-		if(!countUpFinished && window.pageYOffset > statsTopOffset - $(window).height() + 200) {
-			$(".counter").each(function() {
-				let element = $(this);
-				let endVal = parseInt(element.text());
-
-				element.countup(endVal);
-			})
-
-			countUpFinished = true;
-
-		}
+        }
 
 
-	});
+        if (!countUpFinished && window.pageYOffset > statsTopOffset - $(window).height() + 200) {
+            $(".counter").each(function () {
+                let element = $(this);
+                let endVal = parseInt(element.text());
+
+                element.countup(endVal);
+            })
+
+            countUpFinished = true;
+
+        }
 
 
-	$("[data-fancybox]").fancybox();
+    });
 
 
-	$(".items").isotope({
-		filter: '*',
-		animationOptions: {
-			duration: 1500,
-			easing: 'linear',
-			queue: false
-		}
-	});
-
-	$("#filters a").click(function() {
-
-		$("#filters .current").removeClass("current");
-		$(this).addClass("current");
-
-		let selector = $(this).attr("data-filter");
-
-		$(".items").isotope({
-			filter: selector,
-			animationOptions: {
-				duration: 1500,
-				easing: 'linear',
-				queue: false
-			}
-		});
-
-		return false;
-	});
+    $("[data-fancybox]").fancybox();
 
 
+    $(".items").isotope({
+        filter: '*',
+        animationOptions: {
+            duration: 1500,
+            easing: 'linear',
+            queue: false
+        }
+    });
 
-	$("#navigation li a").click(function(e) {
-		e.preventDefault();
+    $("#filters a").click(function () {
 
-		let targetElement = $(this).attr("href");
-		let targetPosition = $(targetElement).offset().top;
-		$("html, body").animate({ scrollTop: targetPosition - 50 }, "slow");
+        $("#filters .current").removeClass("current");
+        $(this).addClass("current");
 
-	});
+        let selector = $(this).attr("data-filter");
 
+        $(".items").isotope({
+            filter: selector,
+            animationOptions: {
+                duration: 1500,
+                easing: 'linear',
+                queue: false
+            }
+        });
 
-
-
-	const nav = $("#navigation");
-	const navTop = nav.offset().top;
-
-	$(window).on("scroll", stickyNavigation);
-
-	function stickyNavigation() {
-
-		let body = $("body");
-
-		if($(window).scrollTop() >= navTop) {
-			body.css("padding-top", nav.outerHeight() + "px");
-			body.addClass("fixedNav");
-		}
-		else {
-			body.css("padding-top", 0);
-			body.removeClass("fixedNav");
-		}
+        return false;
+    });
 
 
+    $("#navigation li a").click(function (e) {
+        e.preventDefault();
+
+        let targetElement = $(this).attr("href");
+        let targetPosition = $(targetElement).offset().top;
+        $("html, body").animate({scrollTop: targetPosition - 50}, "slow");
+
+    });
 
 
-	}
+    const nav = $("#navigation");
+    const navTop = nav.offset().top;
+
+    $(window).on("scroll", stickyNavigation);
+
+    function stickyNavigation() {
+
+        let body = $("body");
+
+        if ($(window).scrollTop() >= navTop) {
+            body.css("padding-top", nav.outerHeight() + "px");
+            body.addClass("fixedNav");
+        } else {
+            body.css("padding-top", 0);
+            body.removeClass("fixedNav");
+        }
+
+
+    }
+
+    $(function () {
+        $('[data-toggle="tooltip"]').tooltip()
+    })
 
 });
 
