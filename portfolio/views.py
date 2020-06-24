986e811946d06a0a00041c43bbb34f1f8d76d0c1
GhitaAlexandruIon <ghita.alexandru6@gmail.com>
2020-06-24 09:27:08 +0300

    fixed skill db import

--- portfolio/views.py
+++ portfolio/views.py
@@ -1,10 +1,11 @@
 from django.shortcuts import render, get_object_or_404
-from.models import Project
+from.models import Project, Skill
 
 
 def home(request):
+    skills = Skill.objects.all()
     projects = Project.objects.all()
-    return render(request, 'portfolio/home.html', {'projects': projects})
+    return render(request, 'portfolio/home.html', {'projects': projects, 'skills': skills})
 
 
 def projects_detail(request, pk):
